758b378bdff2996f0d15d2627cc5bff601f8ca89
Ashwin Kafle <ashwinkafle.teslatech@gmail.com>
2025-12-30 15:51:32 +0545

    arg parsing, proper exception catching, json wire format

--- server.py
+++ server.py
@@ -3,89 +3,129 @@ import asyncio
 import logging
 from websockets.asyncio.server import serve, broadcast
 from collections import defaultdict
+import argparse
+import json
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-async def parse_msg(msg: str) -> tuple[str, str]:
+async def parse_msg(message: str) -> tuple[dict[str, str], bool]:
+    msg: dict[str, str] = {}
+    ok: bool = True
+    def invalid(reason):
+        nonlocal ok
+        msg["reason"] = reason
+        ok = False
     try:
-        action, m = msg.split(' ', maxsplit=1)
-        
+        m = json.loads(message)
+        msg["action"] = m["action"]
+        match m["action"]:
+            case 'privmsg':
+                msg["message"] = m["message"]
+                msg["channel"] = m["channel"]
+            case 'join':
+                msg["channel"] = m["channel"]
+            case 'leave':
+                msg["channel"] = m["channel"]
+            case 'nick':
+                msg["nick"] = m["nick"]
+        for key in ["action", "channel", "nick"]:
+            try:
+                v = m[key]
+                if not isinstance(v, str):
+                    invalid(f"invalid format, '{key}' must be a string")
+                    break
+                if not v.isidentifier():
+                    invalid(f"invalid format, '{key}' must be an identifier")
+                    break
+            except KeyError:
+                    pass
     except ValueError as e:
-        action, m = "invalid", f"invalid format: no action: {e}"
-    return action, m
+        invalid(f"invalid format: {e}")
+    except Exception as e:
+        invalid(f"unknown error: {e}")
+    return (msg, ok)
 
 users: dict = {}
 nicks: dict = {}
 channels: defaultdict[str, set] = defaultdict(set)
 
-async def do_action(websocket, action: str, msg: str) -> tuple[bool, str]:
+async def do_action(websocket, action: str, params: dict[str, str]) -> tuple[bool, str]:
     if websocket not in users and action != "nick":
         return False, "must have a nick"
     if action == "nick":
-        if not msg.isidentifier():
+        wanted_nick = params["nick"]
+        if not wanted_nick.isidentifier():
             return False, "nick must be an identifier"
-        if msg in nicks:
+        if wanted_nick in nicks:
             return False, "nick already exists."
-        logger.info(f"setting nick to {msg}")
-        users[websocket] = {"nick": msg}
-        nicks[msg] = websocket
+        logger.info(f"setting nick to {wanted_nick}")
+        users[websocket] = {"nick": wanted_nick}
+        nicks[wanted_nick] = websocket
         return True, ""
 
     user = users[websocket]
     nick = user['nick']
 
+    if "channel" in params:
+        # ensured above all needed messages actually have this key, and is an identifier
+        chan = params["channel"]
     match action:
         case 'privmsg':
-            try:
-                chan, pmsg = msg.split(' ', maxsplit=1)
-                if len(channels[chan]) == 0:
-                    return False, f"no such channel: {chan}"
-                broadcast(channels[chan], f"{chan}	{nick}: {pmsg}")
-            except ValueError:
-                return False, "invalid syntax, must be: privmsv chan msg..."
+            chan, pmsg = params["channel"], params["message"]
+            if len(channels[chan]) == 0:
+                return False, f"no such channel: {chan}"
+            broadcast(channels[chan], f"{chan}	{nick}: {pmsg}")
         case 'join':
-            if not msg.isidentifier():
-                return False, "channel name must be an identifier"
-            channels[msg].add(websocket)
-            return True, f"joined {msg}"
+            channels[chan].add(websocket)
+            return True, f"joined {chan}"
         case 'leave':
-            channels[msg].discard(websocket)
-            return True, f"removed from {msg}"
-        case 'invalid':
-            return False, msg
+            channels[chan].discard(websocket)
+            return True, f"removed from {chan}"
     return True, ""
 
 
 async def handler(websocket):
     async for message in websocket:
-        action, msg = await parse_msg(message)
+        msg, ok = await parse_msg(message)
         logger.info(f"got msg: {msg}")
-        ok, err = await do_action(websocket, action, msg)
+        if not ok:
+            await websocket.send(f"error: {msg["reason"]}")
+        ok, err = await do_action(websocket, msg["action"], msg)
         if not ok:
             await websocket.send(f"error: {err}")
-    for chan in channels:
+    for chan in channels.values():
         chan.discard(websocket)
     try:
         nick = users[websocket]["nick"]
         del nicks[nick]
         del users[websocket]
         logger.info(f"deleted nick {nick}")
-    except Exception:
-        pass # open-close
+    except KeyError:
+        pass # no nick was defined.
 
-async def main():
-    port=8001
+async def main(host, port):
     logger.info(f"listening on {port}")
     try:
         stop = asyncio.get_event_loop().create_future()
-        async with serve(handler, "", port):
+        async with serve(handler, host, port):
             await stop
     except asyncio.exceptions.CancelledError:
         logger.info("cancelled main!")
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog='chat-websocket',
+        description='A server to chat among multiple channels using websockets',
+    )
+    parser.add_argument('-p', '--port', default=8001, type=int)
+    parser.add_argument('-H', '--host', default="")
+    args = parser.parse_args()
+    return args
+
 if __name__ == "__main__":
-    wlgr = logging.getLogger("websockets")
-    #wlgr.setLevel(logging.DEBUG)
-    asyncio.run(main())
+    # wlgr = logging.getLogger("websockets")
+    # wlgr.setLevel(logging.DEBUG)
+    args = parse_args()
+    asyncio.run(main(host=args.host, port=args.port))
 
